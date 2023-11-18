ecae4076a342f3eb71f6b1bb89162c990f9b68c0
endulum <71853916+endulum@users.noreply.github.com>
2023-11-17 19:28:22 -0800

    read custom error page when file not found

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ http.createServer((req, res) => {
 	fs.readFile(filename, (err, data) => {
 		if (err) {
 			res.writeHead(404, { 'Content-Type': 'text/html' });
-			return res.end('404 Not Found');
+			const errorPage = fs.readFileSync('./pages/404.html', 'utf8');
+			res.write(errorPage);
+			return res.end();
 		}
-
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(data);
 		return res.end();
