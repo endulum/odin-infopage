117f52d8b8fbaa40b5bc74a0760fe3fa201f0526
endulum <71853916+endulum@users.noreply.github.com>
2023-11-22 14:45:24 -0800

    express implemented

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.createServer((req, res) => {
-	const q = url.parse(req.url, true);
-	const filename = "./pages" + q.pathname;
-	// console.log(filename);
-	fs.readFile(filename, (err, data) => {
-		if (err) {
-			res.writeHead(404, { 'Content-Type': 'text/html' });
-			const errorPage = fs.readFileSync('./pages/404.html', 'utf8');
-			res.write(errorPage);
-			return res.end();
-		}
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write(data);
-		return res.end();
-	});
-}).listen(8080);
+app.use(express.static('pages'));
+
+app.all('*', (req, res) => {
+	res.status(500).sendFile('404.html', { root: './pages' });
+});
+
+app.listen(8080);
+
